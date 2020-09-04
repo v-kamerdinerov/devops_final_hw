c4321574504a42a88b2d2e8d8ffe6ccde25e897e
Anakin174 <anakin174jedi@gmail.com>
2020-09-04 13:24:02 +0300

    fix

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt update && sudo apt install -y python docker.io ansible python-pip
+sudo apt update && sudo apt install -y python ansible python-pip
 sudo pip install ansible --upgrade
 #sudo apt install software-properties-common
 #sudo apt-add-repository --yes --update ppa:ansible/ansible
