823ff938695c888aea6f3f0e7331bd62fdb14d03
Anakin174 <anakin174jedi@gmail.com>
2020-09-05 11:18:32 +0300

    fix

--- config.sh
+++ config.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 sudo apt update && sudo apt install -y python python-pip
-sudo pip3 install ansible --upgrade
-#sudo apt install software-properties-common
-#sudo apt-add-repository --yes --update ppa:ansible/ansible
-#sudo apt update
-#sudo apt upgrade
-#sudo apt install ansible -y
 sudo usermod -aG docker ubuntu
 
 
