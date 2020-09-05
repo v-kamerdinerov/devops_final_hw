0c26cc8627144bc680b7ec82d88c78d698f9bf07
Anakin174 <anakin174jedi@gmail.com>
2020-09-05 10:39:28 +0300

    try pip

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt update && sudo apt install -y python python3-pip
+sudo apt update && sudo apt install -y python python-pip
 sudo pip3 install ansible --upgrade
 #sudo apt install software-properties-common
 #sudo apt-add-repository --yes --update ppa:ansible/ansible
